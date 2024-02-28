1e369abc977098ff37d576b217583f24020c0e36
Concision <contact@concision.me>
2024-02-28 02:36:10 -0800

    fix(framework,solutions): resolved formatting and namespacing inconsistencies
    Took 2 minutes
    
    
    Took 27 seconds

--- Solutions/thenintendodude/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Me.TheNintendoDude.ProjectEuler.Solutions;
-
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello world!");
-    }
-}
---
+++ Solutions/thenintendodude/TheNintendoDudeStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Net.ProjectEuler.Framework;
+
+namespace Me.TheNintendoDude.ProjectEuler;
+
+public sealed class ConcisionStartup : ProjectEulerStartup
+{
+    protected override void BootstrapServiceCollection(IServiceCollection serviceCollection)
+    {
+    }
+    
+    public new static async Task<int> Main(params string[] args)
+    {
+        return await Main<ConcisionStartup>(args);
+    }
+}
