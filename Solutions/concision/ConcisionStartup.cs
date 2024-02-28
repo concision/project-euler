9fba1a3dce3306c002308cfa854803707cbcc46a
Concision <contact@concision.me>
2024-02-28 02:02:09 -0800

    refactor: improved configurations of .csproj
    Took 57 hours 57 minutes

--- Solutions/concision/ConcisionStartup.cs
+++ Solutions/concision/ConcisionStartup.cs
@@ -1,19 +1,18 @@
-﻿using Me.Concision.ProjectEuler.Cli;
+﻿using Dev.Concision.ProjectEuler.Cli;
 using Microsoft.Extensions.DependencyInjection;
 using Net.ProjectEuler.Framework;
 using Net.ProjectEuler.Framework.Cli;
-using Net.ProjectEuler.Framework.Cli.Commands;
 
-namespace Me.Concision.ProjectEuler;
+namespace Dev.Concision.ProjectEuler;
 
-public class ConcisionStartup : ProjectEulerStartup
+public sealed class ConcisionStartup : ProjectEulerStartup
 {
     protected override void BootstrapServiceCollection(IServiceCollection serviceCollection)
     {
         serviceCollection.AddScoped<CliCommand, DemoCliCommand>();
     }
 
-    public static async Task<int> Main(params string[] args)
+    public new static async Task<int> Main(params string[] args)
     {
         return await Main<ConcisionStartup>(args);
     }
